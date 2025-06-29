67905ddb45bae81c1d5e9966667878531ca31044
maricielo <cielotapia198@gmail.com>
2025-06-29 10:18:04 -0500

    feat: agregar función para mostrar la lista de productos registrados

--- InventarioProductosVentas.cpp
+++ InventarioProductosVentas.cpp
@@ -17,7 +17,7 @@ struct Venta {
 
 void registrarProducto(Producto productos[], int &cantidad){
 	
-	cout << "Ingrese los datos del producto #" << cantidad + 1 << " a registar :" << endl;
+	cout << "Ingrese los datos del producto #" << cantidad + 1 << " a registrar :" << endl;
 	
 	cout << "\nNombre del producto: ";
 	cin.ignore();
@@ -30,6 +30,18 @@ void registrarProducto(Producto productos[], int &cantidad){
 
 }
 
+void listaProductosRegistrados(Producto productos[], int &cantidad){
+	
+	cout << "Lista de productos registrados:" << endl;
+	
+	for (int i = 0; i < cantidad; i++){
+		cout << "\n  ------------------------ \n";
+		cout << " |" << i + 1 << ". Nombre: " << productos[i].nombre << "    |" << endl;
+		cout << " | precio s/.: " << productos[i].precio << "        |" << endl;
+		cout << "  ------------------------ \n";
+	}
+	
+}
 
 
 void menu(){
@@ -39,19 +51,19 @@ void menu(){
 	int cantidadProducto = 0;
 	
 	do{
-		cout << "\n            ---------------MENU PRINCIPAL---------------\n" << endl;
+		cout << "\n   ---------------MENU PRINCIPAL---------------" << endl;
 			
-		cout << "\n ---------------------------------------------------------------------\n";
-		cout << "  a) Registrar un nuevo producto." << endl;
-		cout << "  b) Listar los productos registrados." << endl;
-		cout << "  c) Buscar un producto por nombre." << endl;
-		cout << "  d) Actualizar los datos de un producto." << endl;
-		cout << "  e) Eliminar un producto." << endl;
-		cout << "  f) Registrar una venta." << endl;
-		cout << "  g) Listar las ventas realizadas." << endl;
-		cout << "  h) Calcular el total de ventas realizadas." << endl;
-		cout << "  s) Salir del programa." << endl;
-		cout << " ---------------------------------------------------------------------\n";
+		cout << "\n  ----------------------------------------------- \n";
+		cout << " | a) Registrar un nuevo producto.               |" << endl;                              
+		cout << " | b) Listar los productos registrados.          |" << endl;
+		cout << " | c) Buscar un producto por nombre.             |" << endl;
+		cout << " | d) Actualizar los datos de un producto.       |" << endl;
+		cout << " | e) Eliminar un producto.                      |" << endl;
+		cout << " | f) Registrar una venta.                       |" << endl;
+		cout << " | g) Listar las ventas realizadas.              |" << endl;
+		cout << " | h) Calcular el total de ventas realizadas.    |" << endl;
+		cout << " | s) Salir del programa.                        |" << endl;
+		cout << "  -----------------------------------------------\n";
 	
 		cout << "\n  Elija una opcion: ";
 		cin >> opcion;
@@ -62,7 +74,8 @@ void menu(){
 				system("pause");
 				break;
 			case 'b':
-				//listaProductosRegistrados();
+				listaProductosRegistrados(productos, cantidadProducto);
+				system("pause");
 				break;
 			case 'c':
 				//buscarProducto();
